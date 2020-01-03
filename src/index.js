eed8f557e0a138198af81ad80f549cc7ff86b6dd
AfterThreeYears <470326964@qq.com>
2020-01-03 16:55:07 +0800

    function Compoent & hook

--- src/index.js
+++ src/index.js
@@ -31,22 +31,34 @@ function createDom(fiber) {
 }
 
 function commitRoot() {
-  deletions.forEach(commitRoot);
+  deletions.forEach(commitWork);
   // TODO add nodes to dom
   commitWork(wipRoot.child);
   currentRoot = wipRoot;
   wipRoot = null;
 }
 
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
+}
+
 function commitWork(fiber) {
   if (!fiber) {
     return;
   }
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+  const domParent = domParentFiber.dom;
   if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom);
+    commitDeletion(fiber, domParent);
   } else if (fiber.effectTag === 'UPDATE' && fiber.dom !== null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   }
@@ -147,15 +159,59 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(fiber) {
+let wipFiber = null;
+let hookIndex = null;
+function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
+  reconcileChildren(fiber, [fiber.type(fiber.props)]);
+}
+
+function useState(initial) {
+  const oldHook = wipFiber.alternate
+    && wipFiber.alternate.hooks
+    && wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(action => hook.state = action(hook.state));
+
+  const setState = action => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  }
+
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
+}
+
+function updateHostComponent(fiber) {
   // todo add dom node
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
-
   // todo create new fiber
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
+  reconcileChildren(fiber, fiber.props.children);
+}
+
+function performUnitOfWork(fiber) {
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
+  }
 
   // todo return next unit of work
   if (fiber.child) {
@@ -174,7 +230,6 @@ function reconcileChildren(wipFiber, elements) {
   let index = 0;
   let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
   let prevSibling = null;
-
   while (index < elements.length || oldFiber != null) {
     // TODO compare oldFiber to element
     const element = elements[index];
@@ -226,22 +281,36 @@ function reconcileChildren(wipFiber, elements) {
 const Fast = {
   createElement,
   render,
+  useState,
 };
 
 const updateValue = e => {
-  console.log('rerender')
-  debugger;
   rerender(e.target.value)
 }
 
 const rerender = value => {
   /** @jsx Fast.createElement */
+
+  function H1(props) {
+    return <h1>{props.name}</h1>
+  }
+  function Counter() {
+    const [state, setState] = Fast.useState(1);
+    return (
+      <h1 onClick={() => setState(c => c + 1)}>
+        Count: {state}
+      </h1>
+    );
+  }
   const element = (
     <div style="background: salmon">
+      <H1 name="Fast react" />
       <input onInput={updateValue} value={value} />
       <h2>Hello {value}</h2>
+      <Counter />
     </div>
   );
+  // const element = <Counter />;
   const container = document.getElementById("root");
   Fast.render(element, container)
 }
